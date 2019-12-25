65906336ce1b96a4a1554e992b893982c2926021
Thomas JEZEQUEL <jezequel.tho@gmail.com>
2019-12-25 22:07:20 +0100

    Initial Commit

---
+++ CarDash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarDash
+//
+//  Created by Thomas Jezequel on 25/12/2019.
+//  Copyright © 2019 Thomas Jezequel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
